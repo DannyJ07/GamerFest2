a60da9128e6e558e677ddc6eb4036bb2493b8129
tamy1304 <tamy13vasconez@gmail.com>
2023-01-11 00:26:35 -0500

    Botones e idioma corregido

--- resources/views/pdfEquipo/index.blade.php
+++ resources/views/pdfEquipo/index.blade.php
@@ -16,7 +16,7 @@
       <div class="container py-5">
           <div class="row">
               <div class="col-xl-12 text-right">
-                  <a href="{{ route('downloadEquipo-pdf') }}" class="btn btn-success btn-sm">Export to PDF</a>
+                  <a href="{{ route('downloadEquipo-pdf') }}" class="btn btn-success btn-sm">Exportar a PDF</a>
               </div>
           </div>
 
